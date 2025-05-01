6d43ea335bbd3e3a0f161636912725124ee94045
Marko-Kinter <kintermarko@gmail.com>
2025-04-30 22:30:55 -0300

    Add: Static Token for users, curl with -H test succeeded

--- applications/users/api.py
+++ applications/users/api.py
@@ -1,8 +1,9 @@
 from .models import User
 from rest_framework import viewsets, permissions
 from .serializer import UserSerializer
+from rest_framework.permissions import BasePermission, IsAuthenticated
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
